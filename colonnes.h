d6027a89aba2ffffd432f977efb2beea95f532ac
Enzo-JZN <130388912+Enzo-JZN@users.noreply.github.com>
2024-04-21 13:03:43 +0200

    Update colonnes.h

--- colonnes.h
+++ colonnes.h
@@ -14,7 +14,7 @@ COLUMN *create_column(char* title);
 int insert_value(COLUMN* col, int value) {
 void delete_column(COLUMN **col);
 void print_col(COLUMN* col);
-int occurence(COLUMN *col, int x);
+int occu_val(COLUMN *col, int x);
 int valeur_presente_x (COLUMN *col, int x);
 int valeur_superieur_x (COLUMN *col, int x);
 int valeur_inferieur_x (COLUMN *col, int x);
