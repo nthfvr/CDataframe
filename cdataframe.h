6ee46dd6b8f98c6217e5577b8155d4e500def198
Enzo-JZN <130388912+Enzo-JZN@users.noreply.github.com>
2024-04-21 13:06:50 +0200

    Update cdataframe.h

--- cdataframe.h
+++ cdataframe.h
@@ -1,1 +1,21 @@
+// Création de l'objet et des attributs d'un CDATAFRAM
+typedef struct {
+    char titre[20];
+    COLUMN **columns; // Cela nous permet ainsi de créer un tableau de colonne, nous déclarons un pointeur vers un tableau de pointeurs vers des colonnes. Cela signifie que nous pouvons stocker plusieurs pointeurs de colonnes dans columns, ce qui nous permet de représenter un tableau dynamique de colonnes.
+    int taille_physique;
+    int taille_logique;
+} CDataframe;
 
+CDataframe* create_CDATAFRAME(char *title);
+CDataframe* create_CDATAFRAME_Saisi(char *title, int taille_logique, int taille_physique, COLUMN** tableau_colonne);
+void afficher_tout_CDATAFRAME (CDataframe* Datafram);
+void afficher_CDATAFRAME_ligne (CDataframe* Datafram, int nb_ligne);
+void afficher_CDATAFRAME_colonne (CDataframe* Datafram, int nb_colonne);
+
+
+// fonctionnalités tache 4 : 
+void Afficher_nb_lignes_Cdataframe (CDataframe* Dataframe);
+void Afficher_nb_colonnes_Cdataframe (CDataframe* Dataframe);
+void valeurs_egale_x_CdataFrame (CDataframe* DataFrame, int x);
+void valeurs_sup_x_CdataFrame (CDataframe* DataFrame, int x);
+void valeurs_inf_x_CdataFrame (CDataframe* DataFrame, int x);
