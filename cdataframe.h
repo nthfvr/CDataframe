2d117549e9b148431ccd0230b7cc53b16b1e419c
Enzo-JZN <130388912+Enzo-JZN@users.noreply.github.com>
2024-04-21 14:01:38 +0200

    Update cdataframe.h

--- cdataframe.h
+++ cdataframe.h
@@ -1,6 +1,11 @@
-#ifndef CDATAFRAME_H
-#define CDATAFRAME_H
 
+
+#ifndef cdataframe_h
+#define cdataframe_h
+
+#include "colonne.h"
+
+// On souhait d'abord créer un objet et une structure colonne :
 // Création de l'objet et des attributs d'un CDATAFRAM
 typedef struct {
     char titre[20];
@@ -9,16 +14,16 @@ typedef struct {
     int taille_logique;
 } CDataframe;
 
-// fonctionnalités 1 : 
+// fonctionnalités 1 :
 CDataframe* create_CDATAFRAME(char *title);
 CDataframe* create_CDATAFRAME_Saisi(char *title, int taille_logique, int taille_physique, COLUMN** tableau_colonne);
 
-// fonctionnalités 2 : 
+// fonctionnalités 2 :
 void afficher_tout_CDATAFRAME (CDataframe* Datafram);
 void afficher_CDATAFRAME_ligne (CDataframe* Datafram, int nb_ligne);
 void afficher_CDATAFRAME_colonne (CDataframe* Datafram, int nb_colonne);
 
-// fonctionnalités 3 : 
+// fonctionnalités 3 :
 
 void ajouter_ligne_valeurs_CDataFrame (CDataframe *DataFrame);
 void supprimer_ligne_valeurs_CDataFrame (CDataframe *DataFrame);
@@ -30,11 +35,11 @@ void remplacer_valeur_CDataFrame (CDataframe* DataFrame, int nouvelle_valeur, in
 
 void afficher_nom_colonne_cdataframe (CDataframe* DataFrame);
 
-// fonctionnalités 4 : 
+// fonctionnalités 4 :
 void Afficher_nb_lignes_Cdataframe (CDataframe* Dataframe);
 void Afficher_nb_colonnes_Cdataframe (CDataframe* Dataframe);
 void valeurs_egale_x_CdataFrame (CDataframe* DataFrame, int x);
 void valeurs_sup_x_CdataFrame (CDataframe* DataFrame, int x);
 void valeurs_inf_x_CdataFrame (CDataframe* DataFrame, int x);
 
-#endif CDATAFRAME_H
+#endif /* cdataframe_h */
