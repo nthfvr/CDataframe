04183adaa2c4c155d28c36e53f107db59ed4a6bd
Enzo-JZN <130388912+Enzo-JZN@users.noreply.github.com>
2024-04-21 13:27:57 +0200

    Update cdataframe.h

--- cdataframe.h
+++ cdataframe.h
@@ -6,14 +6,28 @@ typedef struct {
     int taille_logique;
 } CDataframe;
 
+// fonctionnalités 1 : 
 CDataframe* create_CDATAFRAME(char *title);
 CDataframe* create_CDATAFRAME_Saisi(char *title, int taille_logique, int taille_physique, COLUMN** tableau_colonne);
+
+// fonctionnalités 2 : 
 void afficher_tout_CDATAFRAME (CDataframe* Datafram);
 void afficher_CDATAFRAME_ligne (CDataframe* Datafram, int nb_ligne);
 void afficher_CDATAFRAME_colonne (CDataframe* Datafram, int nb_colonne);
 
+// fonctionnalités 3 : 
 
-// fonctionnalités tache 4 : 
+void ajouter_ligne_valeurs_CDataFrame (CDataframe *DataFrame);
+void supprimer_ligne_valeurs_CDataFrame (CDataframe *DataFrame);
+void ajouter_colonne_valeurs_CDataFrame (CDataframe *DataFrame);
+void supprimer_colonne_valeurs_CDataFrame (CDataframe *DataFrame);
+void renommer_collone_CDataFrame (CDataframe *DataFrame, char* ancien_titre, char* noveau_titre);
+int occurence_valeur_Cdataframe (CDataframe* DataFrame, int x);
+void remplacer_valeur_CDataFrame (CDataframe* DataFrame, int nouvelle_valeur, int num_ligne, int num_colonne);
+
+void afficher_nom_colonne_cdataframe (CDataframe* DataFrame);
+
+// fonctionnalités 4 : 
 void Afficher_nb_lignes_Cdataframe (CDataframe* Dataframe);
 void Afficher_nb_colonnes_Cdataframe (CDataframe* Dataframe);
 void valeurs_egale_x_CdataFrame (CDataframe* DataFrame, int x);
