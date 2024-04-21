634a4b48189bc2d36ca2bc907b78fe20107e349c
Enzo-JZN <130388912+Enzo-JZN@users.noreply.github.com>
2024-04-21 14:03:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include "colonnes.h"
+
 #include "cdataframe.h"
 
-int main()
-{
+//
+// MAIN
+//
 
-    int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]) {
+    
+    // Alloc....
     COLUMN *mycol = create_column("My column");
     insert_value(mycol, 52);
     insert_value(mycol, 44);
@@ -29,17 +33,4 @@ int main()
     afficher_tout_CDATAFRAME(create_CDATAFRAME_Saisi("www", 2, 250, tab));
     afficher_CDATAFRAME_ligne(create_CDATAFRAME_Saisi("www", 2, 250, tab), 2);
     afficher_CDATAFRAME_colonne(create_CDATAFRAME_Saisi("www", 2, 250, tab), 1);
-    
-    COLUMN *mycol = create_column("My column");
-    int val = 5,i;
-    if (insert_value(mycol, val)==1) {
-        printf("Value added successfully to my column\n");
-    }else
-    {printf("Error adding value to my column\n");}
-    print_col(mycol);
-    printf("%d\n",occu_val(mycol, 5));
-    printf("%d\n",val_at_pos(mycol, 0));
-    printf("%d\n",nb_val_inf(mycol, 9));
-    delete_column( &mycol);
-    return 0;
 }
